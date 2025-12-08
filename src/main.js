c8fad36f9fe7e8dd1c5268325e520775d82edc35
Stef-aniya <stefaniyamomot@gmail.com>
2025-12-08 21:59:55 +0200

    hm12

--- src/main.js
+++ src/main.js
@@ -64,12 +64,52 @@ async function onSubmit(event) {
         hideLoader()
     }
 
+// async function onLoadMore() {
+//     currentPage +=1;
+//     showLoader()
+//     try{
+//         const data = await getImagesByQuery(currentQuery,currentPage);
+//         clearGallery(data.hits);
+//         const totalPages = Math.ceil(data.totalHits / 15);
+//         if(currentPage >= totalPages){
+//             hideLoadBtn();
+//             iziToast.info({
+//                 title: "End",
+//                 message: "We're sorry, but you've reached the end of search results."
+//             });
+//         }
+//         smoothScroll();
+//     }catch{
+//         iziToast.error({
+//             title: "Error",
+//             message: "Something went wrong."
+//         });
+//     }finally{
+//         hideLoader
+//     }
+// }
+// function smoothScroll(){
+//     const card = document.querySelector(".gallery-item")
+//     if(!card){
+//         return;
+//     }
+//     const height = card.getBoundingClientRect().height;
+
+//     window.scrollBy({
+//         top: height * 2,
+//         behavior: 'smooth'
+//     });
+// }
+
+}
+
+
 async function onLoadMore() {
     currentPage +=1;
     showLoader()
     try{
         const data = await getImagesByQuery(currentQuery,currentPage);
-        clearGallery(data.hits);
+        createGallery(data.hits);
         const totalPages = Math.ceil(data.totalHits / 15);
         if(currentPage >= totalPages){
             hideLoadBtn();
@@ -85,9 +125,10 @@ async function onLoadMore() {
             message: "Something went wrong."
         });
     }finally{
-        hideLoader
+        hideLoader()
     }
 }
+
 function smoothScroll(){
     const card = document.querySelector(".gallery-item")
     if(!card){
@@ -99,28 +140,4 @@ function smoothScroll(){
         top: height * 2,
         behavior: 'smooth'
     });
-}
-
-
-    getImagesByQuery(query)
-        .then(data => {
-        if (data.hits.length === 0) {
-            iziToast.error({
-            title: 'No results',
-            message: 'Sorry, there are no images matching your search query. Please try again!',
-            });
-            return;
-        }
-
-        createGallery(data.hits);
-        })
-        .catch(() => {
-        iziToast.error({
-            title: 'Error',
-            message: 'Something went wrong. Try again later.',
-        });
-        })
-        .finally(() => {
-        hideLoader();
-        });
 }
